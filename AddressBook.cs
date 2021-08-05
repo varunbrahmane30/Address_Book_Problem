03e44076090201438458b86f36d56d993710bf7e
name <varunbrahmane30@gmail.com>
2021-08-05 16:26:23 +0530

    [varun] added: Storeing details of Person.

---
+++ AddressBook.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookProblemUpdated
+{
+    class AddressBook
+    {
+        Contacts person = new Contacts();
+    }
+}
---
+++ Contacts.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookProblemUpdated
+{
+    class Contacts
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string zipCode { get; set; }
+        public string phoneNunmber { get; set; }
+        public string eMail { get; set; }
+    }
+}
