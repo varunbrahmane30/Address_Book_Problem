8f73cf695936d8e57316b9fdf03de9f99ee982fd
name <varunbrahmane30@gmail.com>
2021-08-19 11:05:59 +0530

    [varun] added: Sql file.

---
+++ SQLQueryAddressBook.sql
@@ -0,0 +1,22 @@
+﻿-- Create Database : 
+
+create database addressbook;
+
+-- Use Database : 
+use addressbook;
+
+-- Creating Table :
+create table AddressBook
+(
+	SNO int identity primary key,
+	FirstName varchar(20),
+	LastName varchar(20),
+	Address varchar(100),
+	City varchar(20),
+	State varchar(20),
+	ZipCode varchar(6),
+	PhoneNumber varchar(10),
+	EmailId varchar(30)
+);
+
+-- Creating Procedure :
